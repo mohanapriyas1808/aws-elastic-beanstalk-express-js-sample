8506818efb31a8a4f616e6dcbc152774676b7d26
mohanapriyas1808 <143476042+mohanapriyas1808@users.noreply.github.com>
2024-11-26 19:30:47 +0530

    Full pipeline test

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('Hi! I am Mohana Priya. I am a passionate cloud and DevOps Engineer!This is my first CD Project'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
