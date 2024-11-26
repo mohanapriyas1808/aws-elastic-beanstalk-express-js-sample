17c1366a593fff7e3a0b908ec3e4cb94e8b2171d
mohanapriyas1808 <mohanapriyas1808@gmail.com>
2024-11-26 18:09:59 +0530

    Full pipelien test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi! I am Mohana Priya. I am a passionate cloud and DevOps Engineer!'));
+app.get('/', (req, res) => res.send('Hi! I am Mohana Priya. I am a passionate cloud and DevOps Engineer!This is my first CD Project'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
